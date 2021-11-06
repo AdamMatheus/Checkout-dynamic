5e43953c995076d1c3362c7d0208c1a0312f749f
AdamMatheus <adam.matheus.it@gmail.com>
2021-11-06 12:03:30 +0100

    ok

--- checkout.js
+++ checkout.js
@@ -7,6 +7,8 @@ window.onload = () => {
 
     window.sessionStorage.setItem("taxRate", taxRate);
     sessionStorage.setItem("shippingPrice", shippingPrice);
+
+    calculateCartTotal();
 }
 
 let quantityControllerDivs = document.getElementsByClassName("quantity-controller");
@@ -39,12 +41,44 @@ const calculateProductTotal = (quantityP) =>{
     // console.log(productPrice);
     // console.log(quantityP.innerText);
     let productTotalPrice = productPrice * parseInt(quantityP.innerText);
-    console.log(productTotalPrice);
+    // console.log(productTotalPrice);
     let productTotalDiv = productInfoDiv.querySelector(".product-line-price");
     productTotalDiv.innerText = productTotalPrice.toFixed(2);
     calculateCartTotal();
 }
 
 const calculateCartTotal = () =>{
-    
+    // NodeList.forEach or array.forEach
+    let productTotalPrices = document.querySelectorAll(".product-line-price");
+
+    // HTML Collection[...].forEach
+    // let productTotalPrices = document.getElementsByClassName("product-line-price");
+
+    let subtotal = 0;
+    console.log(productTotalPrices);
+    productTotalPrices.forEach((productPrice)=>{
+        subtotal += parseFloat(productPrice.innerText);
+    });
+    // console.log(subtotal);
+    // let taxPrice = subtotal * taxRate;
+    let taxPrice = subtotal * parseFloat(localStorage.getItem("taxRate"));
+    let shipping = (subtotal > 0 ? shippingPrice : 0);
+    let cartTotal = subtotal + taxPrice + shipping;
+
+    document.querySelector("#cart-subtotal p:nth-child(2)").innerText = subtotal.toFixed(2);
+    document.querySelector("#cart-tax p:nth-child(2)").innerText = taxPrice.toFixed(2);
+    document.querySelector("#cart-shipping p:nth-child(2)").innerText = shipping.toFixed(2);
+    document.getElementById("cart-total").lastElementChild.innerText = cartTotal.toFixed(2);
+}
+
+document.querySelectorAll(".remove-product").forEach((removeButton)=>{
+    removeButton.addEventListener("click", ()=>{
+        removeProduct(removeButton);
+    });
+});
+
+const removeProduct = (removeButton) =>{
+    let productDiv = removeButton.parentElement.parentElement.parentElement;
+    productDiv.remove();
+    calculateCartTotal();
 }
